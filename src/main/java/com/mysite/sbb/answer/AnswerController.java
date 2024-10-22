24bf8b50881467b1b76571bfd6e322af60615a8f
sooooo56 <sooooo4645@gmail.com>
2024-10-23 00:10:50 +0900

    sbb 2-9 완료

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,10 @@
+package com.mysite.sbb.answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AnswerController {
+
+
+}
---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/question/list")
+    public String list(Model model){
+
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList",questionList);
+
+        return "question_list";
+    }
+}
