b9c5ff3e2a8c7437d5e04eaba4db7ed7181a04e2
sooooo56 <sooooo4645@gmail.com>
2024-10-26 23:01:12 +0900

    sbb 2-10 완료

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,11 +14,21 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    // 목록
     @GetMapping("/question/list")
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
 
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+
+    // 상세보기
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "/question_detail";
     }
 }
