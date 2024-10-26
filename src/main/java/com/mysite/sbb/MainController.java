b1acb065a6284cbd8c3ec904a043b023c4f1b2d4
sooooo56 <sooooo4645@gmail.com>
2024-10-26 22:20:49 +0900

    sbb 2-9 완료

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/sbb")
-    @ResponseBody
-    public String index(){
-        return "index";
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
     }
 }
